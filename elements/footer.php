97c4cc113f0afff21e4abc6845c003ca277479cd
Vburel76 <valentin.burel.76600@gmail.com>
2022-07-04 16:34:29 +0200

    Parameters

---
+++ elements/footer.php
@@ -0,0 +1,16 @@
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top bg-white ">
+    <div class="col-md-4 d-flex align-items-center">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+            <svg class="bi" width="30" height="24">
+                <use xlink:href="#bootstrap" />
+            </svg>
+        </a>
+        <span class="mb-3 mb-md-0 text-muted">&copy; 2022 LA MANU, Inc</span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end fs-3">
+        <i class="bi bi-twitter me-1 mx-1"></i>
+        <i class="bi bi-instagram me-2 mx-1"></i>
+        <i class="bi bi-snapchat me-2 mx-1"></i>
+    </ul>
+</footer>
--- views/home.php
+++ views/home.php
@@ -1,23 +1,26 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<link rel="stylesheet" href="../assets/style/style.css">
-    <title>Flux Rss</title>
-</head>
-<body class ="bg-stadium">
-    <h1>hello world</h1>
-<div class="container bg-light shadow rounded">
-   <h2>hello world 2</h2> 
-   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis laudantium sint quam! Illum voluptatibus, asperiores est dignissimos quibusdam numquam amet voluptate facilis excepturi, vero iusto esse nemo, architecto nostrum cum.</p>
-</div>
+<?php include '../elements/meta.php' ?>
+
+<?php include '../elements/navBar.php' ?>
+
+<body class="bg-stadium ">
+  <h1>hello world</h1>
+  <div class="container bg-light shadow rounded">
+    <h2>hello world 2</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis laudantium sint quam! Illum voluptatibus, asperiores est dignissimos quibusdam numquam amet voluptate facilis excepturi, vero iusto esse nemo, architecto nostrum cum.</p>
+  </div>
 
 
+
+
+
+
+
+
+
+  
+  <?php include '../elements/footer.php' ?>
   <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
+
 </html>
